d298bbbf943a91e54a51520b2fc6de68dc45ebf8
[Esteban Cardona soto] <[estebancardonasoto92@gmail.com]>
2026-02-18 10:51:19 -0500

    Hecho el trabajo de vehiculos clase 3

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,33 @@
 package com.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Objeto 1 usando constructor vacío
+        Vehiculo carro1 = new Vehiculo();
+
+        // Usando setters
+        carro1.setMarca("Mazda");
+        carro1.setModelo("CX-5");
+        carro1.setAnio(2022);
+        carro1.setPrecio(85000);
+
+        // Mostrando con toString
+        System.out.println("Carro 1:");
+        System.out.println(carro1);
+
+        System.out.println("----------------------");
+
+        // Objeto 2 usando constructor con parámetros
+        Vehiculo carro2 = new Vehiculo("Toyota", "Corolla", 2020, 45000);
+
+        // Modificando con setter
+        carro2.setPrecio(43000);
+
+        // Mostrando información
+        System.out.println("Carro 2:");
+        System.out.println(carro2);
     }
 }
+
